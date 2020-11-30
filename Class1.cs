f4e0ac15472ade66e167da441f2b1baae10d18d4
Anurag Patidar <anuragpatidar@yahoo.in>
2020-11-30 16:16:34 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -3,6 +3,10 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using NPOI.SS.UserModel;
+using System.Threading;
+using NPOI.Util;
+using NPOI.XSSF.UserModel;
 using System;
 using System.IO;
 using System.Reflection;
@@ -26,9 +30,16 @@ namespace SeleniumDemo
 
                 // Find the text input element by its name
                 IWebElement query = driver.FindElement(By.Name("q"));
+                
+                //Excel
+                FileStream file = new FileStream("D:\\a\\1\\s\\MDS.xlsx", FileMode.Open, FileAccess.Read);
+                XSSFWorkbook workbook = new XSSFWorkbook(file1);
+                ISheet sheet = workbook.GetSheet("Sheet1");
 
+                var value =string.Format(sheet.GetRow(1).GetCell(0).StringCellValue)
+                
                 // Enter something to search for
-                query.SendKeys("Shell");
+                query.SendKeys(value);
 
                 // Now submit the form. WebDriver will find the form for us from the element
                 query.Submit();
